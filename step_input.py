9a248a0875817c2d8edf23b21e8dd40933c2144a
Akshat Nagar <76489025+akshatnagar50@users.noreply.github.com>
2023-01-09 15:39:16 +0530

    Update step_input.py

--- step_input.py
+++ step_input.py
@@ -2,20 +2,24 @@
 import rospy
 import time
 from plutodrone.msg import PlutoMsg
-def throttle_step():
-    pub=rospy.publisher("/drone_command",PlutoMsg,queue_size=10)
-    rate=rospy.Rate(10)
+def throttle_step(pub):
     obj=PlutoMsg()
+    obj.rcPitch=1500
+    obj.rcRoll=1500
+    obj.rcYaw=1500
+    obj.rcAUX4=1500
     obj.rcThrottle=1825
     pub.publish(obj)
     time.sleep(5)
-    obj.reThrottle=1875
+    obj.rcThrottle=1875
     pub.publish(obj)
     time.sleep(5)
-def pitch_step():
-    pub=rospy.publisher("/drone_command",PlutoMsg,queue_size=10)
-    rate=rospy.Rate(10)
+def pitch_step(pub):
     obj=PlutoMsg()
+    obj.rcPitch=1500
+    obj.rcRoll=1500
+    obj.rcYaw=1500
+    obj.rcAUX4=1500
     obj.rcThrottle=1825
     pub.publish(obj)
     time.sleep(5)    
@@ -23,10 +27,12 @@ def pitch_step():
     pub.publish(obj)
     time.sleep(5)
 
-def roll_step():
-    pub=rospy.publisher("/drone_command",PlutoMsg,queue_size=10)
-    rate=rospy.Rate(10)
+def roll_step(pub):
     obj=PlutoMsg()
+    obj.rcPitch=1500
+    obj.rcRoll=1500
+    obj.rcYaw=1500
+    obj.rcAUX4=1500
     obj.rcThrottle=1825
     pub.publish(obj)
     time.sleep(5)    
@@ -34,10 +40,12 @@ def roll_step():
     pub.publish(obj)
     time.sleep(5)
 
-def yaw_step():
-    pub=rospy.publisher("/drone_command",PlutoMsg,queue_size=10)
-    rate=rospy.Rate(10)
+def yaw_step(pub):
     obj=PlutoMsg()
+    obj.rcPitch=1500
+    obj.rcRoll=1500
+    obj.rcYaw=1500
+    obj.rcAUX4=1500
     obj.rcThrottle=1825
     pub.publish(obj)
     time.sleep(5)    
@@ -48,9 +56,11 @@ def yaw_step():
 if __name__=='__main__':
     try:
         rospy.init_node('step',anonymous=True)  
-        throttle_step()
-        pitch_step()
-        roll_step()
-        yaw_step()
+        pub=rospy.Publisher("/drone_command",PlutoMsg,queue_size=10)
+        rate=rospy.Rate(10)
+        throttle_step(pub)
+        pitch_step(pub)
+        roll_step(pub)
+        yaw_step(pub)
     except rospy.ROSInterruptException:
         pass
