5e34e51fba1423e017290c389e44c61296fcea06
Akshat Nagar <76489025+akshatnagar50@users.noreply.github.com>
2023-01-09 16:45:07 +0530

    Update step_input.py

--- step_input.py
+++ step_input.py
@@ -2,6 +2,18 @@
 import rospy
 import time
 from plutodrone.msg import PlutoMsg
+def arm(pub):
+    obj=PlutoMsg()
+    obj.rcPitch=1500
+    obj.rcRoll=1500
+    obj.rcYaw=1500
+    obj.rcAUX4=1500
+    obj.rcAUX3=1500
+    obj.rcAUX2=1500
+    obj.rcAUX1=1500
+    obj.rcThrottle=1000
+    pub.publish(obj)
+    time.sleep(5)    
 def throttle_step(pub):
     obj=PlutoMsg()
     obj.rcPitch=1500
@@ -72,18 +84,20 @@ if __name__=='__main__':
         rospy.init_node('step',anonymous=True)  
         pub=rospy.Publisher("/drone_command",PlutoMsg,queue_size=10)
         rate=rospy.Rate(10)
+        arm(pub)
         throttle_step(pub)
         land(pub)
-        a=input("HI")
+        time.sleep(10)
+        arm(pub)
         pitch_step(pub)
         land(pub)
-        a=input("HI")
+        time.sleep(10)
+        arm(pub)
         roll_step(pub)
         land(pub)
-        a=input("HI")
+        time.sleep(10)
+        arm(pub)
         yaw_step(pub)
-        land(pub)
-        a=input("HI")
         land(pub)
     except rospy.ROSInterruptException:
         pass
