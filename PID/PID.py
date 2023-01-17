0dbbf9d17bc05f45812c629642b8df447476eb1b
Niyas Mon P <niyasmonp.india@gmail.com>
2023-01-17 23:13:55 +0530

    Update PID.py

--- PID/PID.py
+++ PID/PID.py
@@ -3,6 +3,9 @@ import time
 import rospy
 from plutodrone.msg import PlutoMsg
 from geometry_msgs.msg import PoseStamped
+from std_msgs.msg import Float32
+
+
 class PID:
     """
     Implements a PID controller.
@@ -31,13 +34,18 @@ class PID:
         self.tau = tau
         self.target = target
 
-        self.Dterm = 1000
+        self.Dterm = 0
         self.Iterm = 0
         self.last_error = 0
         self.last_time = time.time()
         self.last_feedback = 0
         self.last_output = 0
-        self.set_limits(0, inf, -inf, inf)
+        self.set_limits(1000, 2000, -inf, inf)
+        rospy.init_node("PID")
+        rospy.Subscriber("Detected",PoseStamped,self.getZ)
+        self.pub=rospy.Publisher("/drone_command",PlutoMsg,queue_size=10)
+        rospy.Subscriber("Kp",Float32,self.setKp)
+
 
     def set_limits(self, min: float, max: float, min_int: float, max_int: float) -> None:
         """
@@ -68,14 +76,13 @@ class PID:
         float
             Output of the PID controller.
         """
-        error = self.target - feedback
+        # print(f"P: {self.Pterm}")  #, I: {self.Iterm}, f: {feedback}
         
         # current_time = time.time()
         # delta_time = 0.001
         # if delta_time == 0:
         #     return self.last_output
 
-        self.Pterm = 1500 + self.Kp * error
         # self.Iterm += (error + self.last_error) * 0.5 * self.Ki * delta_time
         # self.Dterm = (-2 * self.Kd * (feedback - self.last_feedback)
         #               + (2 * self.tau - delta_time) * self.Dterm / (2 * self.tau + delta_time))
@@ -89,8 +96,9 @@ class PID:
         # self.last_error = error
         # self.last_feedback = feedback
 
-        print(f"P: {self.Pterm}")  #, I: {self.Iterm}, f: {feedback}
 
+        error = self.target - feedback
+        self.Pterm = 1500 + self.Kp * error
         output = self.Pterm  #+ self.Iterm + self.Dterm
         if output < self.min:
             return self.min
@@ -103,16 +111,24 @@ class PID:
 
 
 
-def getZ(z_value):
-    current_z = z_value.pose.orientation.x
-    altitude_PID_output = altitude_PID.update(current_z)
-    obj=PlutoMsg()
-    obj.rcPitch=1500
-    obj.rcRoll=1500
-    obj.rcYaw=1500
-    obj.rcAUX4=1500
-    obj.rcThrottle=altitude_PID_output
-    pub.publish(obj)
+    def getZ(self,z_value):
+       current_z = z_value.pose.orientation.x
+       altitude_PID_output = self.update(current_z)
+       obj=PlutoMsg()
+       obj.rcPitch=1500
+       obj.rcRoll=1500
+       obj.rcYaw=1500
+       obj.rcAUX4=1500
+       obj.rcThrottle=altitude_PID_output
+       self.pub.publish(obj)
+
+    def setKp(self,msg):
+        self.Kp=msg.data
+
+    def main(self):
+        rate=rospy.Rate(10)
+        while not rospy.is_shutdown():
+            rate.sleep()
     
 if __name__ == '__main__':
     altitude_Kp = 200
@@ -120,8 +136,7 @@ if __name__ == '__main__':
     altitude_Kd = 0
     target_z  = 1.0
     altitude_PID = PID(altitude_Kp,altitude_Ki,altitude_Kd,target_z,0)
-    altitude_PID.set_limits(1000,2000,0,0)
-    rospy.init_node("PID")
-    rospy.Subscriber("Detected",PoseStamped,callback=getZ)
-    pub=rospy.Publisher("/drone_command",PlutoMsg,queue_size=10)
-
+    altitude_PID.main()
+        
+    # altitude_PID.set_limits(1000,2000,0,0)
+    
