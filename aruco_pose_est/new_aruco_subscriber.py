98689dcb5068614352ca26eeff2f2e7e36c19465
Sukriti Shukla <82026214+Sukriti-Shukla@users.noreply.github.com>
2023-01-17 22:11:07 +0530

    Create new_aruco_subscriber.py

---
+++ aruco_pose_est/new_aruco_subscriber.py
@@ -0,0 +1,11 @@
+import rospy
+from geometry_msgs.msg import PoseStamped
+
+def callback(data):
+    rospy.loginfo(data.pose)
+def listener():
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("Detection", PoseStamped, callback)
+    rospy.spin()
+if __name__ == '__main__':
+    listener()
