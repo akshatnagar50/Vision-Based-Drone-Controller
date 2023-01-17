e73805c59a874cdebbb5a4aaa243107870fe1dc8
Pranit Zope <pranit.t.zope@gmail.com>
2023-01-17 22:16:17 +0530

    slight modifiations to sub and pub

--- aruco_pose_est/new_aruco_subscriber.py
+++ aruco_pose_est/new_aruco_subscriber.py
@@ -1,11 +1,16 @@
 import rospy
 from geometry_msgs.msg import PoseStamped
 
+
 def callback(data):
     rospy.loginfo(data.pose)
+
+
 def listener():
     rospy.init_node('listener', anonymous=True)
     rospy.Subscriber("Detection", PoseStamped, callback)
     rospy.spin()
+
+
 if __name__ == '__main__':
     listener()
