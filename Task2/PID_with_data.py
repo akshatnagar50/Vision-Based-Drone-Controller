42e183184efeb5e9e74efa7b79373e101b8194f2
Chetan Reddy N <60615610+chetanreddy1412@users.noreply.github.com>
2023-02-07 02:08:00 +0530

    Add files via upload

--- Task2/PID_with_data.py
+++ Task2/PID_with_data.py
@@ -62,8 +62,8 @@ class USER_RC():
         self.current_z = 1000
         
         ### DRONE ORIENTATION ###
-        self.orientation_x = None # ROLL
-        self.orientation_y = None # PITCH
+        self.orientation_x = 1000 # ROLL
+        self.orientation_y = 1000 # PITCH
         self.orientation_z = 1000 # YAW
         self.orientation_z_aruco = -1000
 
@@ -189,7 +189,7 @@ class PID:
         self.cam_orientation   = cam_orientation
 
         #Recording the flight data
-        self.df = pd.DataFrame(columns=['rc_th','rc_r','rc_p','rc_y','sen_y','cam_x','cam_y','cam_z''drone_error_x','drone_error_y'])
+        self.df = pd.DataFrame(columns=['rc_th','rc_r','rc_p','rc_y','sen_roll','sen_pitch','sen_yaw','cam_x','cam_y','cam_z''drone_error_x','drone_error_y'])
 
         # self.rc_th = []
         # self.rc_r  = []
@@ -552,7 +552,10 @@ class PID:
         lst=[userRC.rcThrottle,
              userRC.rcRoll ,
              userRC.rcPitch ,
-             userRC.rcYaw ,relative_yaw,
+             userRC.rcYaw ,
+             userRC.orientation_x,
+             userRC.orientation_y,
+             userRC.orientation_z,
              userRC.current_x,
              userRC.current_y,
              userRC.current_z,
@@ -681,22 +684,78 @@ def writeFunction():
             userRC.commandType = NONE_COMMAND
 
 
-        #-------- Reading Yaw Value from IMU ----------#
-        data = client.recv(1024)
-        segments = data.split(b'$M>')
+        # #-------- Reading Yaw Value from IMU ----------#
+        # data = client.recv(1024)
+        # segments = data.split(b'$M>')
         
-        for segment in segments:
-            segment = b'$M>'+segment
-            if len(segment) == 12:
-                if segment[4] == 108:
-                    yaw = segment[9]+256*segment[10] # Little Endian Representation
-                    # roll= segment[5]+256*segment[6]
-                    # pitch = segment[7]+256*segment[8]
-                    # userRC.orientation_x=roll
-                    # userRC.orientation_y=pitch
-                    userRC.orientation_z = yaw
+        # for segment in segments:
+        #     segment = b'$M>'+segment
+        #     if len(segment) == 12:
+        #         if segment[4] == 108:
+        #             yaw = segment[9]+256*segment[10] # Little Endian Representation
+        #             # roll= segment[5]+256*segment[6]
+        #             # pitch = segment[7]+256*segment[8]
+        #             # userRC.orientation_x=roll
+        #             # userRC.orientation_y=pitch
+        #             userRC.orientation_z = yaw
                     
         time.sleep(0.022)
+
+def readFunction():
+    def twos_complement(binary_string): 
+        result = ""
+        # Invert all bits in the binary string
+        for bit in binary_string: 
+            if bit == '0': 
+                result += '1'
+            else: 
+                result += '0'
+        
+    # Add 1 to the result 
+        carry = True
+        for i in range(len(result)-1, -1, -1): 
+            if result[i] == '1' and carry: 
+                result = result[:i] + '0' + result[i+1:]
+                carry = True
+            elif result[i] == '0' and carry: 
+                result = result[:i] + '1' + result[i+1:]
+                carry = False
+    
+        return result
+
+    def twos_complement_inverse(binary_string):
+        if binary_string[0]=='0':
+            return int(binary_string,2)
+        elif binary_string[0]=='1':
+            return -int(twos_complement(binary_string),2)
+        else:
+            print('non binary bit received: ',binary_string[0])
+
+    def get_RPY(segment):
+        #binary representation of the little endian hexadecimal 2 byte value
+        roll_str = (str(bin(segment[6]))[2:].zfill(8)+str(bin(segment[5])[2:].zfill(8)))
+        roll = twos_complement_inverse(roll_str)/10
+
+        pitch_str = str(bin(segment[8]))[2:].zfill(8) + str(bin(segment[7]))[2:].zfill(8)
+        pitch = twos_complement_inverse(pitch_str)/10
+
+        yaw = segment[9] + 256*segment[10]
+
+        return roll,pitch,yaw 
+
+    while True:
+        data = client.recv(1024)
+        
+        segments = data.split(b'$M>')
+        for segment in segments:
+            segment = b'$M>' + segment
+
+            if len(segment)==12: #checking if the length of the packet is 12
+                if segment[4]==108: #checking if the type of the packet is MSP_ATTITUDE
+                    roll,pitch,yaw = get_RPY(segment)
+                    userRC.orientation_x = roll
+                    userRC.orientation_y = pitch
+                    userRC.orientation_z = yaw
 
 def on_press(key):
     # Create a function to handle key presses
@@ -781,6 +840,9 @@ if __name__ == '__main__':
     thread_key = Thread(target=key_handling)
     print('Key Handling Thread Started')
     thread_key.start()
+
+    thread_read = Thread(target=readFunction)
+    thread_read.start()
     
     thread_camera = Thread(target=aruco_detect)
     print('Aruco Detect Thread Started')
